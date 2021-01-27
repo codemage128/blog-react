37ca94cd6bd57b448520dd510fbca333f42f97df
Stitch <futuredev1001@yandex.com>
2021-01-26 20:33:31 -0500

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+export const SERVER_URL = 'http://52.254.23.171:7000/api';
+
+export const LOCAL_URL = 'http://127.0.0.1:7000/api';
+
+export const BASE_URL =
+  process.env.NODE_ENV === 'production' ? SERVER_URL : LOCAL_URL;
+
+export const SYSTEMS_FILE = 'static/excel/systems_dataset_v3.xlsx';
+
+export const USER_FILE = 'static/excel/users_dataset_v2.xlsx';
+
+export const DATA_FILE = 'static/excel/dataset_v3.xlsx';
